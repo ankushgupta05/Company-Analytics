79cc75bda1e3478bf8ad3d9a0f30f0dd3f31653b
Ankush Gupta <144006768+ankushgupta05@users.noreply.github.com>
2024-12-26 11:03:19 +0530

    Delete company_dashboard directory

--- company_dashboard/analytics/urls.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from django.urls import path,include
-from analytics import  views
-urlpatterns = [
-    path('',views.analytics,name='company-name-analytics'),
-]
--- company_dashboard/analytics/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-
-def analytics(request):
-    print("hello ")
-    return render(request,'analytics.html')
-
